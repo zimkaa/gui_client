20f77c3c8b3d67188a6746c6dff3be23641c6d34
Anton Zimin <zimkaa87@gmail.com>
2024-06-04 20:47:56 +0500

    fix: sentry
    Signed-off-by: Anton Zimin <zimkaa87@gmail.com>

--- src/main.py
+++ src/main.py
@@ -1,6 +1,15 @@
 import flet as ft
+import sentry_sdk
 
 from src.application.app import app
+from src.config import settings
+
+
+sentry_sdk.init(
+    dsn=settings.SENTRY,
+    traces_sample_rate=1.0,
+    profiles_sample_rate=1.0,
+)
 
 
 def main() -> None:
